ea07fdb20239a999df3400400d2349e5f4d889be
Matt Russo <matthew.russo91@gmail.com>
2019-12-23 16:21:35 -0500

    variable basics

--- java-basics.java
+++ java-basics.java
@@ -1,5 +1,17 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("this is my first Java program");
-    }
+
+  public static void main(String[] args) {
+      System.out.println("WAGES");
+      
+      Integer hourlyRate;
+      Integer hoursWorked;
+      Integer wages;
+      hourlyRate = 10;
+      hoursWorked = 40;
+      wages = hourlyRate * hoursWorked;
+      
+      System.out.println("$" + wages);
+      
+  }
+
 }
