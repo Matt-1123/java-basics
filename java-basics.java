c94f7a4c816d92f8a2a932b7fb47a49f6b07263f
Matt Russo <matthew.russo91@gmail.com>
2019-12-23 23:36:51 -0500

    Java arrays (ArrayList)

--- java-basics.java
+++ java-basics.java
@@ -1,17 +1,25 @@
+import java.util.*;
 public class HelloWorld {
 
-  public static void main(String[] args) {
-      System.out.println("WAGES");
-      
-      Integer hourlyRate;
-      Integer hoursWorked;
-      Integer wages;
-      hourlyRate = 10;
-      hoursWorked = 40;
-      wages = hourlyRate * hoursWorked;
-      
-      System.out.println("$" + wages);
-      
-  }
+    public static void main(String[] args) {
+        System.out.println("WAGES");
+        
+        Integer hourlyRate;
+        Integer hoursWorked;
+        hourlyRate = 10;
+        hoursWorked = 40;
+        
+        ArrayList employees = new ArrayList<Integer>();
+        employees.add(24);
+        employees.add(4);
+        employees.add(40);
+        System.out.println(employees);
+        
+        Integer wages = hourlyRate * (Integer)employees.get(0);
+        System.out.println("You've worked " + hoursWorked + " hours and made $" + (Integer)employees.get(0) + " this week.");
+        System.out.println("You've worked " + hoursWorked + " hours and made $" + (Integer)employees.get(1) + " this week.");
+        System.out.println("You've worked " + hoursWorked + " hours and made $" + (Integer)employees.get(2) + " this week.");
+
+    }
 
 }
