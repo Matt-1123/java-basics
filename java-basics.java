56052621c8503dd31b12fd9b2ffc0e0bacf7670f
Matt Russo <matthew.russo91@gmail.com>
2019-12-24 00:04:17 -0500

    Java for loop example

--- java-basics.java
+++ java-basics.java
@@ -1,25 +1,25 @@
+package domain;
 import java.util.*;
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("WAGES");
         
+        Integer wages;
         Integer hourlyRate;
-        Integer hoursWorked;
         hourlyRate = 10;
-        hoursWorked = 40;
         
         ArrayList employees = new ArrayList<Integer>();
         employees.add(24);
         employees.add(4);
         employees.add(40);
-        System.out.println(employees);
         
-        Integer wages = hourlyRate * (Integer)employees.get(0);
-        System.out.println("You've worked " + hoursWorked + " hours and made $" + (Integer)employees.get(0) + " this week.");
-        System.out.println("You've worked " + hoursWorked + " hours and made $" + (Integer)employees.get(1) + " this week.");
-        System.out.println("You've worked " + hoursWorked + " hours and made $" + (Integer)employees.get(2) + " this week.");
-
+        Integer i = 0;
+        for (i=0; i<employees.size(); i++) {
+            Integer hoursWorked = (Integer)employees.get(i);
+            wages = hourlyRate * hoursWorked;
+            System.out.println("Employee " + (i+1) + " wages for the week are $" + wages);
+        }
     }
 
 }
