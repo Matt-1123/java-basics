ff32005b923e29db0c8e8b02cd8fb998e2ab7ef8
Matt Russo <matthew.russo91@gmail.com>
2019-12-31 12:02:16 -0500

    Java functions example

--- java-basics.java
+++ java-basics.java
@@ -2,13 +2,33 @@ package domain;
 import java.util.*;
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println("WAGES");
+    public static Integer bonusCalculation(Integer hoursWorked){
+        Integer bonus= 0; //initialize to 0
         
-        Integer wages;
-        Integer hourlyRate;
-        hourlyRate = 10;
+        if(hoursWorked < 10){
+            bonus = 5;
+        } else if(hoursWorked < 30){
+            bonus = 10;
+        } else{
+            bonus = 20;
+        }
         
+        return bonus;
+    }
+    
+    public static Integer calculateWages(Integer hoursWorked){
+        Integer wages = 0;
+        Integer hourlyRate = 10;
+        System.out.println("Hourly rate = " + hourlyRate);
+        
+        Integer bonus = bonusCalculation(hoursWorked);
+        
+        wages = hourlyRate * hoursWorked + bonus;
+        
+        return wages;
+    }
+        
+    public static void main(String[] args){    
         ArrayList employees = new ArrayList<Integer>();
         employees.add(24);
         employees.add(4);
@@ -17,13 +37,8 @@ public class HelloWorld {
         Integer i = 0;
         for (i=0; i<employees.size(); i++) {
             Integer hoursWorked = (Integer)employees.get(i);
-            if(hoursWorked < 10){
-                wages = hoursWorked * hourlyRate + 5;
-            } else if(hoursWorked >= 10 && hoursWorked <= 30){
-                wages = hoursWorked * hourlyRate + 10;
-            } else{
-                wages = hoursWorked * hourlyRate + 20;
-            }
+            Integer wages = calculateWages(hoursWorked);
+            
             System.out.println("Employee " + (i+1) + " wages for the week are $" + wages);
         }
     }
